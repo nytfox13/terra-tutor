32a6a94046b50106070bf1fc01e5ec3015b55f36
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-02-12 19:54:21 +0000

    first terraform commit

---
+++ test.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key =  "AKIAIURYV4FVTN2IR7BA"
+  secret_key =  "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"
+  region = "us-west-2"
+}
+
+resource "aws_instance" "exaample" {
+  ami = "ami-34259654"
+  instance_type = "t2.micro"
+}
